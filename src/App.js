c38401da3967795b0de9a6238030ce50352f3593
Andrew <fedorov-as@bk.ru>
2021-08-20 14:33:03 +0300

    ver05

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import MyComponent from "./MyComponent";
-import MyComponent2 from "./MyComponent2";
+
+function App() {  
+
+  const [inputmessage, setInputMessage]=useState('');
+  console.log(inputmessage);
 
 
+  return  <div className="mainWrapper">
+    <div className='messageList' >
 
-function App() {
-  const [inputText, setInputText] = useState("");
-  const [count, setCount]=useState(0);
-
-console.log('Count - ', count);
-
-const onInputChange = (e) => {
-  setCount(count +1);
-  setInputText(e.target.value);
-}
-
-useEffect (()=>{
-  console.log('Mounted')
-},[])
-
-useEffect (()=>{
-  console.log('onUpdate')
-})
-
-  return (
-    <div>
-      <input
-        value={inputText}
-        onChange={onInputChange}
-        style={{
-          width: 400,
-          height: 100,
-          color: "red",
-        }}
-      />
-
-      <MyComponent textToShow={inputText} />
-      <
     </div>
-  );
+
+      <div className='inputWrapper'>
+        <input className='input' value={inputmessage} onChange={e=>setInputMessage(e.target.value)} />
+        <button>Отправить</button>
+      </div>  
+  </div>
 }
 
 export default App;
